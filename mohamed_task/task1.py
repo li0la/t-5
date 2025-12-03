4963ef3ae2eb01d1c120f1705dc4d6d2eebde927
mido-2005J <hgfvks77cat66@gmail.com>
2025-12-03 17:22:43 +0200

    add task1 file

---
+++ mohamed_task/task1.py
@@ -0,0 +1,26 @@
+class Student ():
+    def __init__(self,name,age):
+        self.__name= name
+        self.__age = age
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name 
+        
+    def get_age (self):
+        return self.__age
+    def set_age (self,age):
+        self.__age=age
+
+st1=Student("mohamed",20 )
+std_name=st1.get_name
+print(std_name())
+st1.set_name ("mohamed")
+print(std_name())
+
+age1=st1.get_age
+std_age=st1.get_age
+print(std_age())
+st1.set_age (50)
+print(std_age())
